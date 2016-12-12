70d174ea1e88c2bba63a1e43bf807d3229633010
@emergentcity <garethsimons@me.com>
2016-12-12 22:14:09 +0000

    more error-handling and constrained db pool size

--- aiovectortiler/db_handler.py
+++ aiovectortiler/db_handler.py
@@ -13,10 +13,19 @@ class DB(object):
         if db_name not in cls._:
             logger.info('adding database {0} at {1}'.format(db_name, dsn_string))
             try:
-                cls._[db_name] = await asyncpg.create_pool(dsn_string, timeout=20)
+                cls._[db_name] = await asyncpg.create_pool(dsn_string, timeout=20, min_size=5, max_size=10)
             except TimeoutError as e:
                 logger.error(e)
-                raise TimeoutError('...unable to connect to db, timeout error, please check your DSN connection string')
+                raise TimeoutError(
+                    '...unable to connect to db, timeout error, \
+                    \n ...please check your DSN connection string, \
+                    \n ...also remember that localhost connections require an IP address if connecting from within docker')
+            except ConnectionRefusedError as e:
+                logger.error(e)
+                raise ConnectionRefusedError(
+                    '...unable to connect to db, connection refused, \
+                    \n ...please check your DSN connection string, \
+                    \n ...also remember that localhost connections require an IP address if connecting from within docker')
         # if this is the first entry and if the name is not default, create a link
         # so doing the first db becomes the default, unless overwritten later by subsequent db pool
         if len(cls._) == 1 and db_name != 'default':
